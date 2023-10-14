729e1bbea5083a715188e23f14fb048643ac9b46
Dustin <asd9532100@gmail.com>
2023-10-14 10:57:56 +0800

    【Feat_新增Action並解釋與View的兩者關聯】 1.先新增Controller 2.新增Action 3.新增View

---
+++ MyWeb/MyController/Controllers/DefaultController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyController.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ShowAry()
+        {
+            int[] score = { 90, 48, 6, 743641, 465, 31, 64, 316, 06, 46, 469, 84861, 465 };
+            int sum = 0;
+            string show = string.Empty;
+            foreach (int i in score)
+            {
+                sum += i;
+                show += i + ", ";
+            }
+            show += "總成績為" + sum;
+            ViewData["Show"] = show;
+            return View();
+        }
+    }
+}
