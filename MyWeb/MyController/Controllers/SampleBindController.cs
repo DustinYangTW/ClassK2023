3b5ba2b7c6fae3d1446530da4e2d34f794304412
Dustin <asd9532100@gmail.com>
2023-10-14 13:38:33 +0800

    【Feat_教學資料洗劫法(簡單細節法)】 1.教學物件導向的多載(Get跟Post)的差異性 2.前端加入Form與method來指定post傳遞資料

---
+++ MyWeb/MyController/Controllers/SampleBindController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyController.Controllers
+{
+    public class SampleBindController : Controller
+    {
+        /// <summary>
+        /// 使用HttpGet
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Create()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 使用HttpPost
+        /// 必須要是Post的請求，才會走這個方法
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult Create(string ID,String Name,int Price)
+        {
+            ViewData["Show"] = $"{ID}, {Name}, {Price}";
+            return View();
+        }
+    }
+}
