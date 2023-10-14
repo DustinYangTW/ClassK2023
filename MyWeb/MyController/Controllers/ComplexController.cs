8ef416d778e6ade1192675fda5990509945763e9
Dustin <asd9532100@gmail.com>
2023-10-14 14:01:42 +0800

    [Feat_複雜細節法] 1.傳遞資料的方式不同(透過Model做傳遞) 2.新增Model 3. 6版的Model需要處理null資料(強制確認，或者把確認的判別關閉) --> <Nullable>disable</Nullable>

---
+++ MyWeb/MyController/Controllers/ComplexController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyController.Models;
+
+namespace MyController.Controllers
+{
+    public class ComplexController : Controller
+    {
+        /// <summary>
+        /// 使用HttpGet
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Create()
+        {
+            return View(new Product());
+        }
+        /// <summary>
+        /// 使用HttpPost
+        /// 必須要是Post的請求，才會走這個方法
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            ViewData["Show"] = $"{product.ID}, {product.Name}, {product.Price}";
+            return View();
+        }
+    }
+}
