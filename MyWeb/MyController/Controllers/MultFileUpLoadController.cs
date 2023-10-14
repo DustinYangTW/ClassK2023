9e088940470c6d0da377b3fb0ecfbbd9eafbeb88
Dustin <asd9532100@gmail.com>
2023-10-14 16:11:00 +0800

    [Feat_教學上傳檔案] --初始大綱 1.資料表上面要變成1對多 2.透過DOM操作，把Input寫入指定區域(全新方法) ------------------------------------------------------ --主要教學 1.透過document.createElement，來新增元素 2.利用陣列來接取多檔案的資料 3.需透過迴圈來處理照片資料

---
+++ MyWeb/MyController/Controllers/MultFileUpLoadController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyController.Controllers
+{
+    public class MultFileUpLoadController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(IFormFile[] Photos)
+        {
+            ViewData["Msg"] = "沒有上傳任何檔案，或者是上傳失敗";
+            if (Photos.Length != 0)
+            {
+                #region 參數初始化，以防止重複宣告
+                FileInfo photoFile;//防止重複宣告
+                string fileNameWithPath = string.Empty;
+                string path = string.Empty;
+                int Count = 0;
+                #endregion
+                FileStream stream;
+                foreach (var photo in Photos)
+                {
+                    //FileName主檔名與副檔名
+                    photoFile = new FileInfo(photo.FileName);
+                    //驗證副檔名
+                    if (photoFile.Extension.ToUpper() == ".JPG" || photoFile.Extension.ToUpper() == ".PNG")
+                    {
+                        path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Photos");
+                        if (!Directory.Exists(path))
+                        {
+                            Directory.CreateDirectory(path);
+                        }
+                        fileNameWithPath = Path.Combine(path, photoFile.Name);
+                        //透過FileStream，複製檔案的地方
+                        using (stream = new FileStream(fileNameWithPath, FileMode.Create))
+                        {
+                            photo.CopyTo(stream);
+                        }
+                    }
+                    Count++;
+                }
+                ViewData["Msg"] = $"{Count}筆上傳成功";
+                return View();
+            }
+            return View();
+        }
+    }
+}
