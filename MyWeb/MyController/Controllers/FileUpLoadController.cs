045d74c774e036263898b2937999d660f5b85270
Dustin <asd9532100@gmail.com>
2023-10-14 15:31:33 +0800

    [Feat_教學上傳檔案] 1.Form表單需要加上【enctype="multipart/form-data"】，不然後端會收不到資料(前端不會拋資料) 2.透過C#內建的【FileInfo】來處理圖片 3.透過【FileInfo.Extension】，來限定某一些格式的檔案 ------------------------------------------- 4.抓取最近的檔案路徑【Path.Combine(Directory.GetCurrentDirectory(),"wwwroot/Photos")】，如果沒有，則新增資料夾 5.利用FileStream上傳檔案 ---------------------------------------------- 重點整理 1.Form表單需要加上【enctype="multipart/form-data"】 2.【Path.Combine(Directory.GetCurrentDirectory(),"wwwroot/Photos")】

---
+++ MyWeb/MyController/Controllers/FileUpLoadController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyController.Controllers
+{
+    public class FileUpLoadController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(IFormFile photo)
+        {
+            ViewData["Msg"] = "沒有上傳任何檔案，或者是上傳失敗";
+            if (photo != null)
+            {
+                //FileName主檔名與副檔名
+                FileInfo photoFile = new FileInfo(photo.FileName);
+                //驗證副檔名
+                if(photoFile.Extension.ToUpper() == ".JPG" ||photoFile.Extension.ToUpper() == ".PNG")
+                {
+                    string path = Path.Combine(Directory.GetCurrentDirectory(),"wwwroot/Photos");
+                    if(!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+                    string fileNameWithPath = Path.Combine(path, photoFile.Name);
+                    //透過FileStream，複製檔案的地方
+                    using (var stream = new FileStream(fileNameWithPath, FileMode.Create))
+                    {
+                        photo.CopyTo(stream);
+                    }
+                    ViewData["Msg"] = "上傳成功";
+                    return View();
+                }
+            }
+            return View();
+        }
+    }
+}
