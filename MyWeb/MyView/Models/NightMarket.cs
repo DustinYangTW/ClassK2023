758901868fb0986aa5988984529f5e8bccee0ba8
Dustin <asd9532100@gmail.com>
2023-10-21 09:03:15 +0800

    【Feat_介紹Model】 1.模型產生的View，一定是跟模型命名 2.中文改寫方式     a.改View -> 需要一個一個改     b.改Model(DataAnnotations)，加入DisPlayName  -> 統一修正 -------------------------------------- 結論 通常情況都會透過讀取Model來顯示資料，只要Model處理的妥當，View都會顯示的很完整

--- MyWeb/MyView/Models/NightMarket.cs
+++ MyWeb/MyView/Models/NightMarket.cs
@@ -1,4 +1,7 @@
-﻿namespace MyView.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyView.Models
 {
     /// <summary>
     /// 可以當成模型來做處理，這種通常都稱為DataModel
@@ -7,7 +10,9 @@
     public class NightMarket
     {
         public string? Id { get; set; }
+        [DisplayName("夜市名稱123465")]
         public string? Name { get; set; }
+        [DisplayName("地址")]
         public string? Address { get; set; }
     }
 }
