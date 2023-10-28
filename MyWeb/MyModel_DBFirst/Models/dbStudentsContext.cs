54009b5c143622edd677c52d2062fa02209c9a8f
Dustin <asd9532100@gmail.com>
2023-10-28 08:19:47 +0800

    【新增dbFirst檔案】

---
+++ MyWeb/MyModel_DBFirst/Models/dbStudentsContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyModel_DBFirst.Models;
+
+public partial class dbStudentsContext : DbContext
+{
+    //public dbStudentsContext(DbContextOptions<dbStudentsContext> options)
+    //    : base(options)
+    //{
+    //}
+
+    //1.2.4 在dbStudentsContext.cs裡撰寫一個空的建構子
+    public dbStudentsContext()
+    { }
+
+
+    //1.2.3 在dbStudentsContext.cs裡撰寫連線到資料庫的程式
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlServer("Data Source=TEACHER;Database=dbStudents;Trusted_Connection=True;TrustServerCertificate=True;User ID=abc;Password=123");
+
+    public virtual DbSet<tStudent> tStudent { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<tStudent>(entity =>
+        {
+            entity.HasKey(e => e.fStuId).HasName("PK__tStudent__08E5BA954E42645E");
+
+            entity.Property(e => e.fStuId)
+                .HasMaxLength(6)
+                .IsUnicode(false)
+                .IsFixedLength();
+            entity.Property(e => e.fEmail).HasMaxLength(40);
+            entity.Property(e => e.fName).HasMaxLength(30);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ MyWeb/MyModel_DBFirst/Models/tStudent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyModel_DBFirst.Models;
+
+public partial class tStudent
+{
+    public string fStuId { get; set; } 
+
+    public string fName { get; set; } 
+
+    public string? fEmail { get; set; }
+
+    public int? fScore { get; set; }
+}
