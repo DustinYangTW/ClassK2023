b4fd165d0ddb49cc8e34029288ebca0db0f520d4
Dustin <asd9532100@gmail.com>
2023-10-28 15:44:36 +0800

    [設定連線資料] 1.在Model建立dbContext，並且繼承dbContext 2.設定Context相關設定 3.設定連線字串 4.*******設定Program，注入連線字串

---
+++ MyWeb/MyModel_CodeFirst/Models/GuestBookContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyModel_CodeFirst.Models
+{
+    public class GuestBookContext : DbContext
+    {
+        //6.1.3 將步驟1.2.4在dbStudentsContext中所寫的空建構子註解掉(也可留著只是用不到)
+        public GuestBookContext(DbContextOptions<GuestBookContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Book> Book { get; set; }
+        public virtual DbSet<ReBook> Department { get; set; }
+    }
+}
--- MyWeb/MyModel_CodeFirst/Program.cs
+++ MyWeb/MyModel_CodeFirst/Program.cs
@@ -1,9 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using MyModel_CodeFirst.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+//Program.cs加入使用appsettings.json中的連線字串程式碼(這段必須寫法var builder這行後面)
+builder.Services.AddDbContext<GuestBookContext>(options =>
+   options.UseSqlServer(builder.Configuration.GetConnectionString("GuestBookConnection"))
+);
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
