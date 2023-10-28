69b8e9e5190f8e121584d6f25a930b1d8a1e1816
Dustin <asd9532100@gmail.com>
2023-10-28 15:24:08 +0800

    [CodeFirst_Model介紹] 1.Model的規則非常重要，會關連到資料庫設定，所以一定要處理好 2.兩張表互相拉關聯 3.設計各個屬性的規則

---
+++ MyWeb/MyModel_CodeFirst/Models/Book.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyModel_CodeFirst.Models
+{
+    public class Book
+    {
+        [Key]
+        public long GId { get; set; }
+        [Display(Name ="主旨")]
+        [Required(ErrorMessage ="必填")]
+        [StringLength(30,ErrorMessage ="主旨不可大於30字")]
+        public string? Title { get; set; }
+        [Display(Name = "內容")]
+        [Required(ErrorMessage = "必填")]
+        [DataType(DataType.MultilineText)] //可以變成多行文字方塊
+        public string? Description { get; set; }
+        [Display(Name = "圖片")]
+        public byte[]? Photo { get; set; }
+        //存放照片Type
+        [Display(Name = "照片類型")]
+        [StringLength(10, ErrorMessage = "照片類型錯誤")]
+        [HiddenInput] //表單是隱藏欄位
+        public string? ImageType { get; set; }
+
+        [Display(Name = "留言人姓名")]
+        [Required(ErrorMessage = "必填")]
+        [StringLength(20, ErrorMessage = "姓名不可大於20個字")]
+        public string? Author { get; set; }
+
+        [Display(Name = "留言時間")]
+        public DateTime TimeStamp { get; set; }
+
+        public List<ReBook>? ReBooks { get; set;}
+    }
+}
---
+++ MyWeb/MyModel_CodeFirst/Models/ReBook.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyModel_CodeFirst.Models
+{
+    public class ReBook
+    {
+        [Key]
+        public long KId { get; set; }
+        [Display(Name = "內容")]
+        [Required(ErrorMessage = "必填")]
+        [DataType(DataType.MultilineText)] //可以變成多行文字方塊
+        public string? Description { get; set; }
+
+        [Display(Name = "留言人姓名")]
+        [Required(ErrorMessage = "必填")]
+        [StringLength(20, ErrorMessage = "姓名不可大於20個字")]
+        public string? Author { get; set; }
+
+        [Display(Name = "留言時間")]
+        public DateTime TimeStamp { get; set; }
+
+        //沒有這樣做會導致關聯拉不上
+
+        [ForeignKey("Book")]
+        [Required]
+        public long GId { get; set; }
+        public virtual Book? Book { get; set; }
+    }
+}
