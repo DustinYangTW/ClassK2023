117a4274a0b0d38a6df59a488d5e4e90385b7eb9
Dustin <asd9532100@gmail.com>
2023-11-04 11:01:36 +0800

    【新增刪除的功能】 1.教學多載的方法 2.教學刪除德基本概念

--- MyWeb/MyModel_CodeFirst/Controllers/BooksController.cs
+++ MyWeb/MyModel_CodeFirst/Controllers/BooksController.cs
@@ -36,99 +36,12 @@ namespace MyModel_CodeFirst.Controllers
         // GET: Books
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Book.ToListAsync());
-        }
+            //2.2.1 改寫Index Action的內容，將留言依新到舊排序
+            var bookList = await _context.Book.OrderByDescending(b => b.GId).ToListAsync();
 
-        // GET: Books/Details/5
-        public async Task<IActionResult> Details(long? id)
-        {
-            if (id == null || _context.Book == null)
-            {
-                return NotFound();
-            }
-
-            var book = await _context.Book
-                .FirstOrDefaultAsync(m => m.GId == id);
-            if (book == null)
-            {
-                return NotFound();
-            }
-
-            return View(book);
-        }
-
-        // GET: Books/Create
-        public IActionResult Create()
-        {
             return View();
         }
 
-        // POST: Books/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("GId,Title,Description,Photo,ImageType,Author,TimeStamp")] Book book)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(book);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(book);
-        }
-
-        // GET: Books/Edit/5
-        public async Task<IActionResult> Edit(long? id)
-        {
-            if (id == null || _context.Book == null)
-            {
-                return NotFound();
-            }
-
-            var book = await _context.Book.FindAsync(id);
-            if (book == null)
-            {
-                return NotFound();
-            }
-            return View(book);
-        }
-
-        // POST: Books/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(long id, [Bind("GId,Title,Description,Photo,ImageType,Author,TimeStamp")] Book book)
-        {
-            if (id != book.GId)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(book);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!BookExists(book.GId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(book);
-        }
 
         // GET: Books/Delete/5
         public async Task<IActionResult> Delete(long? id)
@@ -155,7 +68,7 @@ namespace MyModel_CodeFirst.Controllers
         {
             if (_context.Book == null)
             {
-                return Problem("Entity set 'GuestBookContext.Book'  is null.");
+                return Problem("這裡有一些些小問題發生哦....");
             }
             var book = await _context.Book.FindAsync(id);
             if (book != null)
