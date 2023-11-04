b308014a83ceca61eeffc52a3b08849cef04e211
Dustin <asd9532100@gmail.com>
2023-11-04 16:18:48 +0800

    【3.5   回覆留言功能，教學Ajax、製作前後端分離的回覆留言功能、Ajax教學】 1.在Display新增mdal，並讀取相關寫入資料 -----製作前後端分離的回覆留言功能---- 2.透過JS，把後端的View寫到前端     ---透過.load()，把資料寫近來---- 3.再透過Ajajx，把新增資料送到後端處理 4.下週再教學

---
+++ MyWeb/MyModel_CodeFirst/Controllers/RePostBooksController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MyModel_CodeFirst.Models;
+
+namespace MyModel_CodeFirst.Controllers
+{
+    public class RePostBooksController : Controller
+    {
+        private readonly GuestBookContext _context;
+
+        public RePostBooksController(GuestBookContext context)
+        {
+            _context = context;
+        }
+
+     
+
+        // GET: RePostBooks/Create
+        public IActionResult Create(long id)
+        {
+            //ViewData["GId"] = new SelectList(_context.Book, "GId", "Author");
+
+            ViewData["GId"] = id;
+
+            return View();
+        }
+
+        // POST: RePostBooks/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("RId,Description,Author,TimeStamp,GId")] ReBook reBook)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(reBook);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["GId"] = new SelectList(_context.Book, "GId", "Author", reBook.GId);
+            return View(reBook);
+        }
+
+        
+    }
+}
