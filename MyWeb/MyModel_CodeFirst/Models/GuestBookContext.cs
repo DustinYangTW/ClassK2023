9a9369b3cca5ce1c0b37b025a7d82f3f6a8dd3c5
Dustin <asd9532100@gmail.com>
2023-11-18 10:46:46 +0800

    Add-Migration AddLogInTable

--- MyWeb/MyModel_CodeFirst/Models/GuestBookContext.cs
+++ MyWeb/MyModel_CodeFirst/Models/GuestBookContext.cs
@@ -1,16 +1,23 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
+//1.2.2 撰寫GuestBookContext類別的內容
 namespace MyModel_CodeFirst.Models
 {
-    public class GuestBookContext : DbContext
+    //(1)須繼承DbContext類別
+    public class GuestBookContext:DbContext
     {
-        //6.1.3 將步驟1.2.4在dbStudentsContext中所寫的空建構子註解掉(也可留著只是用不到)
+        //(2)撰寫建構子(使用依賴注入DI)
         public GuestBookContext(DbContextOptions<GuestBookContext> options)
-            : base(options)
+       : base(options)
         {
         }
 
+        //(3)描述資料庫裡面的資料表
         public virtual DbSet<Book> Book { get; set; }
         public virtual DbSet<ReBook> ReBook { get; set; }
+
+        //4.1.4 修改GuestBookContext類別的內容，加入描述資料庫裡Login的資料表
+        public virtual DbSet<Login> Login { get; set; }
+
     }
 }
